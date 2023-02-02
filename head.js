220c51337f284bcde4ddff3519a8b0bbcfa4a5f4
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-02 08:22:22 -0500

    performed eslint and made some changes

--- head.js
+++ head.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
   } else {
@@ -6,13 +6,13 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const head = function (array) {
+const head = function(array) {
   if (array === []) {
     return undefined;
   } else {
     return array[0];
   }
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
