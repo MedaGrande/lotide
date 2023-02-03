458539d3aa974bd9c0147e2d2d52d1620a2ec224
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-02 23:46:26 -0500

    eslint

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
   } else {
@@ -10,11 +10,11 @@ const assertEqual = function (actual, expected) {
 //allItems: an array of strings that we need to look through
 //itemsToCount: an object specifying what to count
 
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
+    //console.log(item);
     if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
@@ -25,7 +25,7 @@ const countOnly = function (allItems, itemsToCount) {
   }
 
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
