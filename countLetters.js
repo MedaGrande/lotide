2c5d8e4b1719769e49525c16ddda2acbc5223e6f
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-03 18:38:42 -0500

    solved the space issue with .replaceAll

--- countLetters.js
+++ countLetters.js
@@ -7,19 +7,17 @@ const assertEqual = function (actual, expected) {
 };
 
 const countLetters = function (str) {
+  str = str.replaceAll(' ', '');
   const counts = {};
   const newStr = str.split('');
   for (const letter of newStr) {
-    if (counts[letter] !== ' ') {
       if (counts[letter]) {
         counts[letter] += 1;
       } else {
         counts[letter] = 1;
       }
     }
-  }
   console.log(counts);
-      
 };
 
 countLetters("test for today");
