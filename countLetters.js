df47e2a164e8dd2872262b4a8e3a692c255a546e
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-03 18:25:03 -0500

    working on removing spaces from the splitted string

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,21 @@ const assertEqual = function (actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function (str) {
+  const counts = {};
+  const newStr = str.split('');
+  for (const letter of newStr) {
+    if (counts[letter] !== ' ') {
+      if (counts[letter]) {
+        counts[letter] += 1;
+      } else {
+        counts[letter] = 1;
+      }
+    }
+  }
+  console.log(counts);
+      
+};
+
+countLetters("test for today");
