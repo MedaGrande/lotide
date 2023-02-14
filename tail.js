3107ce4e42694b0b33ba41e2687bae0429e3e1d5
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-14 17:14:07 -0500

    refactored tail function and separated the test section

--- tail.js
+++ tail.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   const noHeadArray = array.slice(1);
   return noHeadArray;
 };
+module.exports = tail;
 
-console.log(tail([5, 6, 7, 8]));
-console.log(tail([5]));
-console.log(tail([]));
 
-const test1 = [1, 2, 3];
-tail(test1);
-assertEqual(test1.length, 3);
