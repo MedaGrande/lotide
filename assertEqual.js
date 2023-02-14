edbd9819eaaafba4455bac57ab8aeceb14b2fdcd
Medhanie Alene <alenemedhanie@gmail.com>
2023-02-13 23:14:09 -0500

    function refactored by using require and module to separate tests

--- assertEqual.js
+++ assertEqual.js
@@ -5,8 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+module.exports = assertEqual;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Hello", "Hello");
-assertEqual(2, 4);
